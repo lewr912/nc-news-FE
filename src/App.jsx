06f9c365920852e4e17bbe7d6af291c55361790c
lewis912 <lewiscc93@gmail.com>
2025-11-10 15:51:59 +0000

    add BrowserRouter, MainView route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import MainView from "./pages/MainView";
+import "./App.css";
 
 function App() {
   return (
-      <div>HELLO</div>
-  )
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<MainView />} />
+      </Routes>
+      
+    </div>
+  );
 }
 
-export default App
+export default App;
