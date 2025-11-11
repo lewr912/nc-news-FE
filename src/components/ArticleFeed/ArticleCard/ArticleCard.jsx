4dc47b986cb06381fa36c620fbb2f12af52cd3dd
lewis912 <lewiscc93@gmail.com>
2025-11-11 14:35:04 +0000

    add links to individual articles on /articles/:article_id route

--- src/components/ArticleFeed/ArticleCard/ArticleCard.jsx
+++ src/components/ArticleFeed/ArticleCard/ArticleCard.jsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
 import "./ArticleCard.css"
 
 function ArticleCard({articleDetails}) {
     
     
-    const {title, author, topic, votes, comment_count, created_at, article_img_url} = articleDetails;
+    const {title, author, topic, votes, comment_count, created_at, article_img_url, article_id} = articleDetails;
     
     return  <li className="articleCard">
+        <Link to={`/articles/${article_id}`}>EXPAND ARTICLE</Link>
         <h3>{title}</h3>
         <div>{author}  </div>
         <div>{topic} </div>
