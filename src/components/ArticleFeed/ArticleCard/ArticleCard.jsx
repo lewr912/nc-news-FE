30bba0ca84411e7ae19199ae80a06c5dd975f754
lewis912 <lewiscc93@gmail.com>
2025-11-14 11:32:26 +0000

    move articleCard element render to articleFeed, fix article links

--- src/components/ArticleFeed/ArticleCard/ArticleCard.jsx
+++ src/components/ArticleFeed/ArticleCard/ArticleCard.jsx
@@ -1,46 +1,11 @@
 import { Link } from "react-router-dom";
 import "./ArticleCard.css";
-import { convertTimeStamp } from "../../../utils/utils";
 
-function ArticleCard({ articleDetails }) {
-  const {
-    title,
-    author,
-    topic,
-    votes,
-    comment_count,
-    created_at,
-    article_img_url,
-    article_id,
-  } = articleDetails;
-
+function ArticleCard({ children, article_id }) {
+ 
   return (
     <li className="articleCard">
-      <Link to={`/articles/${article_id}`}>Expand</Link>
-      <section>
-        <ul>
-          <h3>{title}</h3>
-          <li>
-            <u>User</u> {author}
-          </li>
-          <li>
-            <u>Topic</u> {topic}{" "}
-          </li>
-        </ul>
-      </section>
-      <ul className="bottomRow">
-        <li>
-          <u>Votes</u> {votes}
-        </li>
-        <li>
-          <u>Comments</u> {comment_count}
-        </li>
-        <li>
-          <u>Created</u> {convertTimeStamp(created_at)}
-        </li>
-      </ul>
-
-      <img className="image" src={`${article_img_url}`} />
+      <Link to={`/articles/${article_id}`}>{children}</Link>
     </li>
   );
 }
--- src/components/ArticleFeed/ArticleFeed.jsx
+++ src/components/ArticleFeed/ArticleFeed.jsx
@@ -1,25 +1,43 @@
 import { useEffect, useState } from "react";
-import ArticleCard from "./ArticleCard/ArticleCard"
-import "./ArticleFeed.css"
+import ArticleCard from "./ArticleCard/ArticleCard";
+import { convertTimeStamp } from "../../utils/utils";
+import "./ArticleFeed.css";
 
+convertTimeStamp;
 function ArticleFeed() {
-    const [articleList, setArticleList] = useState([])
-    useEffect(() => {
-        fetch(`https://newsit-xcqx.onrender.com/api/articles`)
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            const { articles } = data;
-            setArticleList(articles);
-        })
-    }, [])
-    
-    return <ul id="articleFeed">
-        {articleList.map((article) => {
-            return <ArticleCard key={article.article_id} articleDetails={article}/>
-        })}
+  const [articleList, setArticleList] = useState([]);
+  useEffect(() => {
+    fetch(`https://newsit-xcqx.onrender.com/api/articles`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const { articles } = data;
+        setArticleList(articles);
+      });
+  }, []);
+
+  return (
+    <ul id="articleFeed">
+      {articleList.map((article) => {
+        return (
+          <ArticleCard key={article.article_id} article_id={article.article_id}>
+            {" "}
+            <div>
+              <u>Topic</u> {article.topic} <h3>{article.title}</h3>
+            </div>
+            <div className="bottomRow">
+              <u>Votes</u> {article.votes}
+              <u>Comments</u> {article.comment_count}
+              <u>Created</u> {convertTimeStamp(article.created_at)}
+            </div>
+            <u>User</u> {article.author}
+            <img className="image" src={`${article.article_img_url}`} />{" "}
+          </ArticleCard>
+        );
+      })}
     </ul>
+  );
 }
 
-export default ArticleFeed
+export default ArticleFeed;
