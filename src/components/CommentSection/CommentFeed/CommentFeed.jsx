6417c49630094f87c864960548133a0b6f7982c3
lewis912 <lewiscc93@gmail.com>
2025-11-14 10:10:25 +0000

    move comment fetching from commentFeed to its parent component

--- src/components/CommentSection/CommentFeed/CommentFeed.jsx
+++ src/components/CommentSection/CommentFeed/CommentFeed.jsx
@@ -1,30 +1,11 @@
-import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
 import CommentCard from "./CommentCard/CommentCard";
 import "./CommentFeed.css";
 
-function CommentFeed() {
-  const { article_id } = useParams();
-  const [commentsForArticle, setCommentsForArticle] = useState([]);
-
-  useEffect(() => {
-    fetch(
-      `https://newsit-xcqx.onrender.com/api/articles/${article_id}/comments`
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const { comments } = data;
-        setCommentsForArticle(comments);
-      })
-  }, [commentsForArticle]);
-
+function CommentFeed({ commentsForArticle }) {
   return (
     <ul id="commentFeed">
       {commentsForArticle.map((comment) => {
-        const { body, votes, author, created_at, comment_id} = comment;
-        return <CommentCard key={comment_id} commentDetails={comment} />;
+        return <CommentCard key={comment.comment_id} commentDetails={comment} />;
       })}
     </ul>
   );
--- src/components/CommentSection/CommentSection.jsx
+++ src/components/CommentSection/CommentSection.jsx
@@ -1,13 +1,52 @@
 import CommentFeed from "./CommentFeed/CommentFeed";
 import NewCommentForm from "./NewCommentForm/NewCommentForm";
 import "./CommentSection.css";
+import { useEffect, useState } from "react";
 
-function CommentSection({article_id}) {
+function CommentSection({ article_id, onCommentSubmit }) {
+  const [commentsForArticle, setCommentsForArticle] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  function fetchCommentsForArticle() {
+    setIsLoading(true);
+    fetch(
+      `https://newsit-xcqx.onrender.com/api/articles/${article_id}/comments`
+    )
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        const { comments } = data;
+        setCommentsForArticle(comments);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+      });
+  }
+  useEffect(() => {
+    fetchCommentsForArticle();
+  }, []);
+
+  function handleNewComment() {
+    fetchCommentsForArticle();
+  }
+
+  if (isLoading) {
+    return <div>Loading comments</div>;
+  }
+
   return (
-    <section id="commentSection">
-      <NewCommentForm article_id={article_id}/>
-      <CommentFeed />
-    </section>
+    <div id="commentSection">
+      <NewCommentForm
+        article_id={article_id}
+        onSubmitSuccess={onCommentSubmit}
+        onCommentRefetch={handleNewComment}
+      />
+      <CommentFeed commentsForArticle={commentsForArticle} />
+    </div>
   );
 }
 
