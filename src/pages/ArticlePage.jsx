c7159359f9a1337f3e160bdb00e7ba66e245b5f9
lewis912 <lewiscc93@gmail.com>
2025-11-14 10:18:57 +0000

    add loading state and callback to handle a new submitted comment

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -3,28 +3,49 @@ import ArticleContent from "../components/ArticleContent/ArticleContent";
 import { useParams } from "react-router-dom";
 import CommentSection from "../components/CommentSection/CommentSection";
 
-
 function ArticlePage() {
   const [articleData, setArticleData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const { article_id } = useParams();
 
-  useEffect(() => {
+  function fetchArticleData() {
+    setIsLoading(true);
     fetch(`https://newsit-xcqx.onrender.com/api/articles/${article_id}`)
       .then((response) => {
-        return response.json();
+        if (response.ok) {
+          return response.json();
+        }
       })
       .then((data) => {
         const { article } = data;
         setArticleData(article);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setIsLoading(false);
       });
+  }
+  useEffect(() => {
+    fetchArticleData();
   }, []);
 
+  function handleSubmittedComment() {
+    fetchArticleData();
+  }
+
+  if (isLoading) {
+    return <div>Loading content</div>;
+  }
+
   return (
-    <main>
+    <div>
       article page
       <ArticleContent articleData={articleData} />
-      <CommentSection article_id={article_id}/>
-    </main>
+      <CommentSection
+        article_id={article_id}
+        onCommentSubmit={handleSubmittedComment}
+      />
+    </div>
   );
 }
 
